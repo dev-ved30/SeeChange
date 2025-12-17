0f70acf5d9c57157bd997daa5f04b266b228d844
ved <vedshah.email@gmail.com>
2025-12-17 13:11:08 -0600

    Basic function for asteroid checking should be implemented now.

--- pipeline/asteroid_checker.py
+++ pipeline/asteroid_checker.py
@@ -1,103 +1,89 @@
 import kete
-import psycopg2
 
 import numpy as np
 import astropy.units as u
 
-from astropy.coordinates import SkyCoord, match_coordinates_sky
-from astropy.io import fits
-from astropy.wcs import WCS
+from astropy.coordinates import SkyCoord
+from astropy.time import Time
 
-from pipeline.parameters import Parameters
+from models.base import PsycopgConnection
 
-default_radius_threshold = 30 * u.arcsec
-chip_fov_radius = 1 * u.deg
+default_radius_threshold = 30*u.arcsec
+chip_fov_radius = 1*u.deg
 
-db_params = {
-    "dbname": "asteroid",
-    "user": "vedshah",  # Change to your PostgreSQL username
-    "host": "localhost",  # Change if PostgreSQL is hosted remotely
-    "port": "5432"  # Default PostgreSQL port
-}
+def download_and_update_db(time_jd):
 
-def download_and_update_db(time_jd, db_params=db_params):
+    with PsycopgConnection() as conn:
 
-    # Establish connection
-    conn = psycopg2.connect(**db_params)
-    cursor = conn.cursor()
+        cursor = conn.cursor()
 
-    # Load orbit data from the MPC
-    mpc_obs = kete.mpc.fetch_known_orbit_data(force_download=True)
+        # Load orbit data from the MPC
+        mpc_obs = kete.mpc.fetch_known_orbit_data(force_download=True)
 
-    # Convert that data to State objects.
-    mpc_states = kete.mpc.table_to_states(mpc_obs)
+        # Convert that data to State objects.
+        mpc_states = kete.mpc.table_to_states(mpc_obs)
 
-    # Update the states based on the current time. 
-    mpc_states_jd = kete.propagate_n_body(mpc_states, time_jd)
+        # Update the states based on the current time. 
+        mpc_states_jd = kete.propagate_n_body(mpc_states, time_jd)
 
-    # Update the states for the next day. This will be used to find the proper motion.
-    mpc_states_jd_plus_1 = kete.propagate_n_body(mpc_states_jd, time_jd + 1)
+        # Update the states for the next day. This will be used to find the proper motion.
+        mpc_states_jd_plus_1 = kete.propagate_n_body(mpc_states_jd, time_jd + 1)
 
-    # Convert the states from ecliptic to equatorial to easily access the RA and Dec.
-    for s_jd, s_jd_plus_1 in zip(mpc_states_jd, mpc_states_jd_plus_1):
+        # Convert the states from ecliptic to equatorial to easily access the RA and Dec.
+        for s_jd, s_jd_plus_1 in zip(mpc_states_jd, mpc_states_jd_plus_1):
 
-        # Convert the state to equatorial coordinates.
-        ra_jd = s_jd.as_equatorial.pos.ra 
-        dec_jd = s_jd.as_equatorial.pos.dec 
+            # Convert the state to equatorial coordinates.
+            ra_jd = s_jd.as_equatorial.pos.ra 
+            dec_jd = s_jd.as_equatorial.pos.dec 
 
-        ra_jd_plus_1 = s_jd_plus_1.as_equatorial.pos.ra 
-        dec_jd_plus_1 = s_jd_plus_1.as_equatorial.pos.dec 
+            ra_jd_plus_1 = s_jd_plus_1.as_equatorial.pos.ra 
+            dec_jd_plus_1 = s_jd_plus_1.as_equatorial.pos.dec 
 
-        # Compute the average proper motion for the next day. In deg/day.
-        pm_ra = ra_jd_plus_1 - ra_jd
-        pm_dec = dec_jd_plus_1 - dec_jd
-        
-        # Add the state to the database. 
-        # TODO: Add them to a temporary table first and then replace the main table.
-        cursor.execute("INSERT INTO asteroids (designation, jd, ra, dec, pm_ra, pm_dec, position_x, position_y, position_z, velocity_x, velocity_y, velocity_z) VALUES \
-                       (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", \
-                       (s_jd.desig, s_jd.jd, ra_jd, dec_jd, pm_ra, pm_dec, s_jd.pos.x, s_jd.pos.y, s_jd.pos.z, s_jd.vel.x, s_jd.vel.y, s_jd.vel.z))
+            # Compute the average proper motion for the next day. In deg/day.
+            pm_ra = ra_jd_plus_1 - ra_jd
+            pm_dec = dec_jd_plus_1 - dec_jd
+            
+            # Add the state to the database if it doesn't exist, otherwise update it.
+            cursor.execute("""
+                INSERT INTO mpc_table (designation, jd, ra, dec, pm_ra, pm_dec, 
+                                    position_x, position_y, position_z, 
+                                    velocity_x, velocity_y, velocity_z) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT (designation) DO UPDATE SET
+                    jd = EXCLUDED.jd,
+                    ra = EXCLUDED.ra,
+                    dec = EXCLUDED.dec,
+                    pm_ra = EXCLUDED.pm_ra,
+                    pm_dec = EXCLUDED.pm_dec,
+                    position_x = EXCLUDED.position_x,
+                    position_y = EXCLUDED.position_y,
+                    position_z = EXCLUDED.position_z,
+                    velocity_x = EXCLUDED.velocity_x,
+                    velocity_y = EXCLUDED.velocity_y,
+                    velocity_z = EXCLUDED.velocity_z
+                """, (s_jd.desig, s_jd.jd, ra_jd, dec_jd, pm_ra, pm_dec, 
+                    s_jd.pos.x, s_jd.pos.y, s_jd.pos.z, 
+                    s_jd.vel.x, s_jd.vel.y, s_jd.vel.z)
+                )
 
-    conn.commit()
+        conn.commit()
 
-    # Close the connection
-    cursor.close()
-    conn.close()
-
-class ParsAsteroidChecker(Parameters):
-    def __init__(self, **kwargs):
-        super().__init__()
-
-        self.radius_threshold = self.add_par(
-            'radius_threshold',
-            30,
-            float,
-            'Radius threshold in arcseconds for matching asteroids to sources.',
-            critical=False
-        )
-
-        self._enforce_no_new_attrs = True
-        self.override(kwargs)
-
-    def get_process_name(self):
-        return 'asteroid_checking'
 
 class AsteroidChecker:
 
-    def __init__(self, **kwargs):
+    def __init__(self):
         
         # Load the database table and convert to states
-        self.pars = ParsAsteroidChecker(**kwargs) 
         self._load_states_from_db()
 
-    def _load_states_from_db(self, db_params=db_params):
+    def _load_states_from_db(self):
 
-        try:
-            conn = psycopg2.connect(**db_params)
+        with PsycopgConnection() as conn:
+
             cursor = conn.cursor()
 
             # Only select objects such that ra/dec + proper motion is within the field of view.
-            cursor.execute("SELECT * FROM asteroids;")
+            cursor.execute("SELECT * FROM mpc_table;")
             rows = cursor.fetchall()
 
             # Convert rows into sate objects
@@ -109,23 +95,27 @@ class AsteroidChecker:
                                 kete.vector.Vector([velocity_x, velocity_y, velocity_z]))
                 self.mpc_states.append(state)
             
-            # Close the connection
-            cursor.close()
-            conn.close()
-
-        except Exception as e:
-            print("Error connecting to database:", e)    
 
     def run(self, ds):
-        
-        # NOTE: This is the pseudocode structure of the dataset 'ds'.
-        wcs, time = ds['wcs'], ds['time_jd']
-        mpc_skycoords, mpc_designations = self._get_asteroid_list(wcs, time)
 
-        sources = ds['sources']
-        sources_skycoords = SkyCoord(sources['ra'], sources['dec'], unit="deg")
+        measurement_set = ds.get_measurement_set()
+        if measurement_set is None:
+            raise ValueError( f'Cannot find a measurement set corresponding to '
+                                f'the datastore inputs: {ds.inputs_str}' )
+        measurements = measurement_set.measurements
 
-        matched_asteroids = self._cross_match_sources_with_asteroids(mpc_skycoords, mpc_designations, sources_skycoords)
+        # Use the measurements to get the center estimate of the field of view
+        measurement_ras = [m.ra for m in measurements]
+        measurement_decs = [m.dec for m in measurements]
+        measurement_skycoords = SkyCoord(measurement_ras, measurement_decs, unit="deg")
+
+        time_mjd = ds.get_image().mjd
+        time_jd = Time(time_mjd, format='mjd').jd
+        wcs = ds.get_image().wcs
+
+        mpc_skycoords, mpc_designations = self._get_asteroid_list(wcs, time_jd)
+
+        ds.mpc_designations = self._cross_match_sources_with_asteroids(mpc_skycoords, mpc_designations, measurement_skycoords)
 
     
     def _get_asteroid_list(self, frame_wcs, time_jd):
@@ -176,15 +166,10 @@ class AsteroidChecker:
 
     def _cross_match_sources_with_asteroids(self, asteroid_sky_coords, asteroid_designations, sources_sky_coords, radius_threshold=default_radius_threshold):
 
-        matched_asteroids = []
-        idx, d2d, _ = sources_sky_coords.match_to_catalog_sky(asteroid_sky_coords)
-
-        # Process and display the cross-match results
-        for source, match_index, separation in zip(sources_sky_coords, idx, d2d):
-
-            if separation < radius_threshold:
-                matched_asteroids.append(asteroid_designations[match_index])
-            else:
-                matched_asteroids.append(None)
+        if len(asteroid_sky_coords) == 0:
+            return [None] * len(sources_sky_coords)
         
+        idx, d2d, _ = sources_sky_coords.match_to_catalog_sky(asteroid_sky_coords)
+        matched_asteroids = np.array([asteroid_designations[i] if d2d[j] < radius_threshold else None 
+                                      for j, i in enumerate(idx)])
         return matched_asteroids
